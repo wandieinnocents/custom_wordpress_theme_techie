0fc35fff08dc4c8729a79a12845524860a5efe1c
wandieinnocents <wandieinnocent2@gmail.com>
2023-06-27 15:52:29 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+/**
+ * Main tempelate file
+ * 
+ * @package Techie
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Techie</title>
+</head>
+<body>
+
+<h1>This is wordpress theme</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
